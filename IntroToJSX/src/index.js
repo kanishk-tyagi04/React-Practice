f14d0a2db45bb25832dadc1a13164597828034bb
Kanishk Tyagi <111213277+kanishk-tyagi04@users.noreply.github.com>
2025-04-20 14:28:13 +0530

    Add files via upload

---
+++ IntroToJSX/src/index.js
@@ -0,0 +1,9 @@
+var React = require("react");
+var ReactDOM = require("react-dom");
+ReactDOM.render(
+  <div>
+    <h1>Hello World</h1>
+    <p>This is a Paragraph</p>
+  </div>,
+  document.getElementById("root")
+);
